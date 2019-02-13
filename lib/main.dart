a7c28d0baa5b3c5bedb9824c338f19eee2faee99
Alexandre Bianchi <alexandre.bianchi@ab-inbev.com>
2019-02-13 15:47:13 -0200

    Chapter 31 completed

--- lib/main.dart
+++ lib/main.dart
@@ -1,53 +1,19 @@
 import 'package:flutter/material.dart';
 
+import './product_manager.dart';
+
 void main() => runApp(MyApp());
 
-class MyApp extends StatefulWidget {
-  @override
-  State<StatefulWidget> createState() {
-    return _MyAppState();
-  }
-}
-
-class _MyAppState extends State<MyApp> {
-  List<String> _products = ['Food Tester'];
-
+class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-          appBar: AppBar(
-            title: Text('EasyList'),
-          ),
-          body: Column(
-            children: [
-              Container(
-                margin: EdgeInsets.all(10.0),
-                child: RaisedButton(
-                  onPressed: () {
-                    setState(() {
-                      _products.add('Advanced Food Tester');
-                    });
-                  },
-                  child: Text('Add Product'),
-                ),
-              ),
-              Column(
-                children: _products
-                    .map(
-                      (element) => Card(
-                            child: Column(
-                              children: <Widget>[
-                                Image.asset('assets/food.jpg'),
-                                Text('Food Paradise')
-                              ],
-                            ),
-                          ),
-                    )
-                    .toList(),
-              )
-            ],
-          )),
+        appBar: AppBar(
+          title: Text('EasyList'),
+        ),
+        body: ProductManager(),
+      ),
     );
   }
 }
