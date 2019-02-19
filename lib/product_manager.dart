9691a1f3ba64ca1e789340783101cd82d9d2bca3
Alexandre Bianchi <xandebianchi@gmail.com>
2019-02-19 14:11:23 -0300

    Chapter 36 concluded

--- lib/product_manager.dart
+++ lib/product_manager.dart
@@ -5,10 +5,13 @@ import './products.dart';
 class ProductManager extends StatefulWidget {
   final String startingProduct;
 
-  ProductManager(this.startingProduct);
+  ProductManager({this.startingProduct = 'Sweets Tester'}) {
+    print('[ProductManager Widget] Constructor');
+  }
 
   @override
   State<StatefulWidget> createState() {
+    print('[ProductManager Widget] createState()');
     return _ProductManagerState();
   }
 }
@@ -18,17 +21,26 @@ class _ProductManagerState extends State<ProductManager> {
 
   @override
   void initState() {
-    super.initState();
+    print('[ProductManager State] initState()');
     _products.add(widget.startingProduct);
+    super.initState();
+  }
+
+  @override
+  void didUpdateWidget(ProductManager oldWidget) {
+    print('[ProductManager State] didUpdateWidget()');
+    super.didUpdateWidget(oldWidget);
   }
 
   @override
   Widget build(BuildContext context) {
+    print('[ProductManager State] build()');
     return Column(
       children: [
         Container(
           margin: EdgeInsets.all(10.0),
           child: RaisedButton(
+            color: Theme.of(context).primaryColor,
             onPressed: () {
               setState(() {
                 _products.add('Advanced Food Tester');
