822155712cb01bd8c6e1d8619dedef7e6cfba5fb
Alexandre Bianchi <xandebianchi@gmail.com>
2019-02-26 16:13:21 -0300

    Chapter 73 ended

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 // import 'package:flutter/rendering.dart';
 
-import './pages/home.dart';
+import './pages/auth.dart';
 
 void main() {
   // debugPaintSizeEnabled = true;
@@ -20,7 +20,7 @@ class MyApp extends StatelessWidget {
         primarySwatch: Colors.deepOrange,
         accentColor: Colors.deepPurple
       ),
-      home: HomePage()
+      home: AuthPage()
     );
   }
 }
