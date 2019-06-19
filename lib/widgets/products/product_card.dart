3af2b1ae4d99bb278ba00184b3c1c5844741d176
Alexandre Bianchi <xandebianchi@gmail.com>
2019-06-19 12:44:00 -0300

    newinfo

---
+++ lib/widgets/products/product_card.dart
@@ -0,0 +1,63 @@
+import 'package:flutter/material.dart';
+
+import './price_tag.dart';
+
+class ProductCard extends StatelessWidget {
+  final Map<String, dynamic> product;
+  final int productIndex;
+
+  ProductCard(this.product, this.productIndex);
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      child: Column(
+        children: <Widget>[
+          Image.asset(product['image']),
+          Container(
+              padding: EdgeInsets.only(top: 10.0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  Text(
+                    product['title'],
+                    style: TextStyle(
+                        fontSize: 26.0,
+                        fontWeight: FontWeight.bold,
+                        fontFamily: 'Oswald'),
+                  ),
+                  SizedBox(
+                    width: 8.0,
+                  ),
+                  PriceTag(product['price'].toString())
+                ],
+              )),
+          Container(
+            padding: EdgeInsets.symmetric(horizontal: 6.0, vertical: 2.5),
+            decoration: BoxDecoration(
+                border: Border.all(color: Colors.grey, width: 1.0),
+                borderRadius: BorderRadius.circular(4.0)),
+            child: Text('Union Square, San Fancisco'),
+          ),
+          ButtonBar(
+            alignment: MainAxisAlignment.center,
+            children: <Widget>[
+              IconButton(
+                icon: Icon(Icons.info),
+                color: Theme.of(context).accentColor,
+                onPressed: () => Navigator.pushNamed<bool>(
+                    context, '/product/' + productIndex.toString()),
+              ),
+              IconButton(
+                icon: Icon(Icons.favorite_border),
+                color: Colors.red,
+                onPressed: () => Navigator.pushNamed<bool>(
+                    context, '/product/' + productIndex.toString()),
+              )
+            ],
+          )
+        ],
+      ),
+    );
+  }
+}
